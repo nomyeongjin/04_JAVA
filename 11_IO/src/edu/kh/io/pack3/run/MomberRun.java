3b676a92426f4a395a6fdd2e5c76e0514a4f24b5
nomyeongjin <bbagifa2555@gmail.com>
2024-02-15 16:41:43 +0900

    240215_입출력

---
+++ 11_IO/src/edu/kh/io/pack3/run/MomberRun.java
@@ -0,0 +1,12 @@
+package edu.kh.io.pack3.run;
+
+import edu.kh.io.pack3.model.service.ObjectService;
+
+public class MomberRun {
+	public static void main(String[] args) {
+		ObjectService service = new ObjectService();
+		
+//		service.ObjectOutput();
+		service.ObjectInput();
+	}
+}
